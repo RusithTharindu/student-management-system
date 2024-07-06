0655fb68623deade564387e992789bd7ed7552d9
Thushan W A R T <rusiththarindu2@gmail.com>
2024-07-06 16:20:11 +0530

    backend: MongoDB configuration

---
+++ BACKEND/server.js
@@ -0,0 +1,33 @@
+//dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+require("dotenv").config(); //reads the .env files
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL; //defining mongo url
+
+mongoose.connect(URL, {
+    //DB connection options
+    // useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false 
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+    console.log("Successfully connected to MongoDB !");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port: ${PORT}`);
+});
